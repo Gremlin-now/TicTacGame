ba8ebc5a126044bc47ad3446801116beab5d0fe1
Giessoo <66471094+Gremlin-now@users.noreply.github.com>
2023-10-06 20:23:44 +0300

    NEO PROPEY TABLETKI
    Сделал выбор крестика или нолика в самом начале игры

--- script.js
+++ script.js
@@ -8,13 +8,35 @@ let fieldButt = [
     document.getElementById("C1"),
     document.getElementById("C2"),
     document.getElementById("C3")
-]
+];
+let field_line = document.getElementsByClassName("field__line");
 let counter = 0;
-fieldButt.forEach(element => {
-    console.log(element)
+
+let playersMark = ["O","O"];
+
+let choiceButtContainer = [
+    document.getElementById("X"),
+    document.getElementById("O")
+];
+choiceButtContainer.forEach(element => {
     element.addEventListener("click", function(e){
-        counter++
-        this.innerText = (counter%2==1)?"X":"O";
+        if(this.innerText == "X"){
+            playersMark[0] = "X";
+        }else{
+            playersMark[1] = "X";
+        }
+        for(let i = 0; i < field_line.length; i++)
+        {
+            field_line[i].style.display = "grid";
+        }
+        document.getElementById("choice").style.display = "none";
+    }, {once:true});
+});
+
+fieldButt.forEach(element => {
+    element.addEventListener("click", function(e){
+        counter++;
+        this.innerText = (counter%2==1)?playersMark[0]:playersMark[1];
         this.disabled = true;
     }, {once:true});
 });
